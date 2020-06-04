e8790fd177d3d3a1c7b553f82d4feadb3351cdf9
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-04 11:32:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,10 +9,11 @@ end
 
 def add_element_to_end_of_array(array, element)
   
+  array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
-  array << element
+
 end
 
 def remove_element_from_end_of_array(array)
